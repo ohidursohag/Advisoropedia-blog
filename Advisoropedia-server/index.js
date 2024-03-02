61d8b0866575f8fbe4c38833288208840648d0bc
“ohidursohag” <ohidursohag9595@gmail.com>
2024-03-02 16:05:04 +0600

    Created User login route, checking user validation, generate jwt access token. create logout route fo clear access token when a user logged out

--- Advisoropedia-server/index.js
+++ Advisoropedia-server/index.js
@@ -26,7 +26,8 @@ async function main() {
 
 // Register route
 app.post("/advisoropedia/api/v1/register", async (req, res) => {
-  const { fullName, email, profileImage, password, userRole } = req.body;
+  const { fullName, email, profileImage, password, userRole } =
+    req.body;
 
   try {
     // console.log('register hit')
@@ -59,11 +60,78 @@ app.post("/advisoropedia/api/v1/register", async (req, res) => {
 });
 
 
+// Login route
+app.post("/advisoropedia/api/v1/login", async (req, res) => {
+  const { email, password } = req.body;
+  console.log(req.body);
+  try {
+    console.log("Login end hit");
+    // Find user by email
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // Check password
+    const validPassword = await bcrypt.compare(password, user.password);
+    console.log(validPassword);
+    if (!validPassword) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    // Create and send JWT token
+    const token = jwt.sign(
+      {
+        fullName: user.fullName,
+        email: user.email,
+        profileImage: user.profileImage,
+        userRole: user.userRole,
+        phoneNumber: user.phoneNumber,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+      })
+      .send({ success: true, token });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+// Clear access token when user logged out
+app.get("/advisoropedia/api/v1/logout", async (req, res) => {
+  try {
+    res
+      .clearCookie("accessToken", {
+        maxAge: 0,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+      })
+      .send({ success: true });
+  } catch (error) {
+    return res.send({ error: true, error: error.message });
+  }
+});
+
+
+
+
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
-  res.send("Hello from Advisoropedia Server..");
-});
-
-app.listen(port, () => {
-  console.log(`Advisoropedia is running on port ${port}`);
-});
+   res.send("Hello from Advisoropedia Server..");
+ });
+ 
+ app.listen(port, () => {
+   console.log(`Advisoropedia is running on port ${port}`);
+ });
